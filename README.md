102e7d3a64970cfbec51a7446cf1b0fc750cdc9c
Midelight <61563710+Hypenexy@users.noreply.github.com>
2023-05-05 00:40:24 +0300

    Everything overhaul!

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # NodeJS-FileExplorer
-A simple file explorer written in NodeJS and runs in an http server.
+A simple file explorer written in NodeJS and runs in an http server.  
 Don't actually use it in production.
 
 ![image](https://user-images.githubusercontent.com/61563710/236160832-f74d059e-2050-47d3-bbbc-26f7b678ecd4.png)
--- index.js
+++ index.js
@@ -6,21 +6,41 @@ var http, server;
 http = require('http');
 server = http.createServer(function (req, res) {
     // console.log('An http user connected');
-    if(req.url=="/style.css"){
+    if(req.url=="/assets/styles/style.css"){
         res.writeHead(200, {
             'Content-Type': 'text/css'
         });
-        fs.readFile("style.css", (error, content) => {
+        fs.readFile("assets/styles/style.css", (error, content) => {
             res.write(content);
             res.end();
         });
         return;
     }
-    if(req.url=="/folder.svg"){
+    if(req.socket.remoteAddress != "::1" && req.socket.remoteAddress != "::ffff:127.0.0.1"){
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        fs.readFile("assets/pages/500.html", (error, content) => {
+            res.write(content);
+            res.end();
+        });
+        return;
+    }
+    if(req.url=="/assets/images/folder.svg"){
         res.writeHead(200, {
             'Content-Type': 'image/svg+xml'
         });
-        fs.readFile("folder.svg", (error, content) => {
+        fs.readFile("assets/images/folder.svg", (error, content) => {
+            res.write(content);
+            res.end();
+        });
+        return;
+    }
+    if(req.url=="/assets/fonts/Material-Icons-Outlined.woff2"){
+        res.writeHead(200, {
+            'Content-Type': 'font/woff2'
+        });
+        fs.readFile("assets/fonts/Material-Icons-Outlined.woff2", (error, content) => {
             res.write(content);
             res.end();
         });
@@ -31,10 +51,28 @@ server = http.createServer(function (req, res) {
         res.end();
         return;
     }
-    res.write("<link rel='stylesheet' href='/style.css'>")
     req.url = req.url.replaceAll("%20", " ");
-    res.write(iterateFolder(req.url));
-    res.end();
+    const files = iterateFolder(req.url);
+    var navbarHTML = "<a href='/'>Drive</a>";
+    const directories = req.url.split('/');
+    for (let i = 1; i < directories.length; i++) {
+        const element = directories[i];
+        var pathUntilFolder = '';
+        for (let m = 1; m < i; m++) {
+            if(directories[m]){
+                pathUntilFolder += directories[m] + "/";
+            }
+        }
+        navbarHTML += `<i>chevron_right</i> <a href="/${pathUntilFolder}${element}">${element}</a>`;
+    }
+    // res.write(files);
+    fs.readFile("assets/pages/client.html", (error, content) => {
+        content = content.toString();
+        content = content.replaceAll("{$files}", files);
+        content = content.replaceAll("{$navbar}", navbarHTML);
+        res.write(content);
+        res.end();
+    });
 });
 
 
@@ -68,27 +106,59 @@ function iterateFolder(folder){
             webPath = path.slice(1, path.length);
         }
 
-        var isDir;
+        var fileStats 
         try {
-            isDir = fs.lstatSync(path).isDirectory();
+            fileStats = fs.lstatSync(path);
         } catch (error) {}
+
+        var isDir, size
+        if(fileStats){
+            isDir = fileStats.isDirectory();
+            size = fileStats.size;
+        }
+        // if(!isDir)
 
         return {
             name: file,
             path: path,
             webPath: webPath,
-            isDir: isDir
+            isDir: isDir,
+            size: size
         };
     })
     .sort((a, b) => b.isDir - a.isDir || a.name - b.name);
 
     directoryInfo.forEach(file => {
         if(file.isDir){
-            html += `<a href='${file.webPath}'><img src='/folder.svg'>${file.name}</a>`;
+            html += `<a href='${file.webPath}'><i>folder</i>${file.name}</a>`;
         }
         else{
-            html += `<a>${file.name}</a>`;
+            html += `<a><p>${file.name}</p><p>${humanFileSize(file.size)}</p></a>`;
         }
     });
     return html;
+}
+
+
+// From MDUtils.js
+function humanFileSize(bytes, si=true, dp=1) {
+    const thresh = si ? 1000 : 1024;
+
+    if (Math.abs(bytes) < thresh) {
+      return bytes + ' B';
+    }
+
+    const units = si 
+      ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] 
+      : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+    let u = -1;
+    const r = 10**dp;
+
+    do {
+      bytes /= thresh;
+      ++u;
+    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+
+
+    return bytes.toFixed(dp) + ' ' + units[u];
 }
